c59ff4db4a64c11a533d35043c7759f220b08a21
Dzmitry Shaplyka <Dzmitry_Shaplyka@epam.com>
2020-03-15 19:05:35 +0300

    small refactoring

--- test/helpers/request.helper.js
+++ test/helpers/request.helper.js
@@ -2,13 +2,13 @@ const fetch = require('node-fetch');
 const endpoint = require('../../config/endpoint/endpoint');
 
 const getEmployees = async () => {
-    const response = await fetch(endpoint.url + 'employees');
+    const response = await fetch(`${endpoint.url}/employees`);
     return { cookie: response.headers.raw()['set-cookie'], body: await response.json() };
 };
 
-const getEmployee = async (id) => {
+const getEmployee = async id => {
     const employees = await getEmployees();
-    const response = await fetch(`${endpoint.url}employee/${id}`, { headers: { 'Cookie': employees.cookie } });
+    const response = await fetch(`${endpoint.url}/employee/${id}`, { headers: { 'Cookie': employees.cookie } });
     return response.json();
 };
 
