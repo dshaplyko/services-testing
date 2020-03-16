e6f072daa7f91c92b9f3e36f6da83125f11daeae
Dzmitry Shaplyka <Dzmitry_Shaplyka@epam.com>
2020-03-16 11:15:16 +0300

    create post test added

--- test/helpers/request.helper.js
+++ test/helpers/request.helper.js
@@ -3,6 +3,7 @@ const endpoint = require('../../config/endpoint/endpoint');
 const HttpsProxyAgent = require('https-proxy-agent');
 const proxy =  new HttpsProxyAgent('http://127.0.0.1:8888');
 
+//https://jsonplaceholder.typicode.com/guide.html
 const getPosts = async () => {
     const response = await fetch(`${endpoint.url}/posts`, {agent: proxy});
     return response.json();
@@ -14,7 +15,12 @@ const getPost = async id => {
 };
 
 const createPost = async post => {
-
+    const response = await fetch(`${endpoint.url}/posts/`, {
+        method: 'POST',
+        body: JSON.stringify(post),
+        agent: proxy
+    });
+    return response.json();
 };
 
 const updatePost = async post => {
--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -1,28 +1,38 @@
 const requestHelper = require('../helpers/request.helper');
+const data = require('../../config/data/test');
 
 describe('Test suite', () => {
 
   before(() => {
     //to avoid https:// issue
-    NODE_TLS_REJECT_UNAUTHORIZED=0;
+    NODE_TLS_REJECT_UNAUTHORIZED = 0;
   });
 
   it('Get all Posts', async () =>  {
-    const response = await requestHelper.getPosts();
-    console.log(response);
+    const posts = await requestHelper.getPosts();
+    
+    expect(posts.length).to.be.above(0);
+    posts.forEach(post => {
+      expect(typeof post.id).to.equal('number');
+      expect(typeof post.userId).to.equal('number');
+      expect(typeof post.body).to.equal('string');
+      expect(typeof post.title).to.equal('string');
+    });
+  });
+
+  it('Create a Post', async () =>  {
+    const post = await requestHelper.createPost(data);
+    expect(post.id).to.equal(101);
   });
 
   it('Get a single Post', async () =>  {
-    const response = await requestHelper.getPost(1);
-    console.log(response);
-  });
-
-
-
-  it('Create a Post', async () =>  {
+    const post = await requestHelper.getPost(100);
     
+    expect(post.id).to.equal(100);
+    expect(typeof post.userId).to.equal('number');
+    expect(typeof post.body).to.equal('string');
+    expect(typeof post.title).to.equal('string');
   });
-    
 
   it('Update a post', async () => {
    
